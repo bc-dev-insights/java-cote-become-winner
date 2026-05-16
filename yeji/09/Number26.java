376f23bf7e18cdd6736707fd001c05b0b1035fa7
cyeji <98408267+cyeji@users.noreply.github.com>
2026-05-16 23:03:31 +0900

    Add solution method to determine match rounds
    예상 대진표

---
+++ yeji/09/Number26.java
@@ -0,0 +1,28 @@
+class Solution{
+    
+    public int solution(int n, int a, int b){
+        int count = 0;
+        
+    
+        while(a != b){
+            
+            if(a%2 == 0){
+                
+                a = a/2;
+                
+            }else{
+                a= a/2 + 1;
+            }
+            
+            if(b%2 == 0){
+                b = b/2;
+            }else{
+                b = b/2 + 1;
+            }
+            
+            count++;
+        }
+        
+        return count;
+    }
+}
